31794d240fdba29423fb736512838968323a160e
DylanGriffioenHU <43435932+DylanGriffioenHU@users.noreply.github.com>
2019-03-14 16:10:43 +0100

    Opdracht_00.cpp
    Opdracht_00.cpp

--- opdracht_00.cpp
+++ opdracht_00.cpp
@@ -1,18 +1,30 @@
+string.size()/2
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
+std::string translate(std::string variabele, std::string input)
+{
+    std::string new_string ="";
+    int myasciival = 0;
 
-int main(int argc, char * argv[]){
-    string input = argv[1];
-    int argument = int(*argv[2] - '0');
+    for(int i=0; i < input.size(); i++){
+        myasciival = int(input[i]) + std::stoi(variabele);
+        char newChar = myasciival;
+        new_string.insert(i,1,newChar);}
+   return new_string;
+}
 
-    for (size_t i = 0; i <= input.size(); i++){
-         input[i] += argument;
-    }
+int main(int argc, char *argv[])
+{
+  std::string s;
 
-     cout << "De encrypte zin is: " << input << endl;
+  if(argc != 2)
+  { std::cerr << "Deze functie heeft exact 1 argument nodig";
+    return -1; }
+
+  while(true)
+  { std::cin >> s;
+    if(std::cin.eof()){ return 0; }
+  std::cout << translate(argv[1], s) << std::endl;
+  }
 }
